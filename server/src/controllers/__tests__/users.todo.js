4ca68b3c04d50dbeb32e60b8d3b6f1b05ac04617
Khizer Rehan <khizer.rehan@aurorasolutions.io>
2020-07-13 00:02:02 +0500

    completed: mocking

--- server/src/controllers/__tests__/users.todo.js
+++ server/src/controllers/__tests__/users.todo.js
@@ -7,8 +7,25 @@
 /*
 http://ws.kcd.im/?ws=Testing&e=users%20test$20object%20factories&em=
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
-  expect(submitted).toBe(true)
+
+import {initDb} from 'til-server-test-utils'
+import * as usersController from '../users'
+import db from '../../utils/db'
+import {omit} from 'lodash'
+
+const safeUser = user => omit(user, ['salt', 'hash'])
+
+beforeEach(() => initDb())
+
+test('getUsers should return all the users in the database', async () => {
+  const req = {}
+  const res = {
+    json: jest.fn(),
+  }
+
+  await usersController.getUsers(req, res)
+  expect(res.json).toHaveBeenCalledTimes(1)
+
+  const actualUsers = await db.getUsers
+  expect(actualUsers).toEqual(actualUsers.map(safeUser))
 })
-////////////////////////////////
